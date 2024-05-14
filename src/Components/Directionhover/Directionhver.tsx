919cf3247693d93e9d54948ffbb922236d435e57
haziq <haziq.elitx@gmail.com>
2024-05-14 21:39:19 +0500

    pics width are set

--- src/Components/Directionhover/Directionhver.tsx
+++ src/Components/Directionhover/Directionhver.tsx
@@ -1,19 +1,16 @@
 import { DirectionAwareHover } from "../ui/direction-aware-hover";
-// import vedio1 from "../../assets/cream style.mp4"; 
 import { CardStackDemo } from "../Cardstack/Cardstack";
-// import vedio2 from "../../assets/fd-case3 (1).mp4"
-// import vedio3 from "../../assets/fd-case2 (1).mp4"
+
 const DirectionAwareHoverDemo = () => {
   const images = [
-    "https://res.cloudinary.com/dzo4lqjb2/image/upload/v1715052158/SCR-20240402-qdaq_nqwv6a.jpg",
-    "https://res.cloudinary.com/dzo4lqjb2/image/upload/v1715052230/SCR-20240402-qjdq_teltfq.jpg",
-    "https://res.cloudinary.com/dzo4lqjb2/image/upload/v1715053159/SCR-20240507-kodp_u6owdk.jpg",
+    "https://images.unsplash.com/photo-1663765970236-f2acfde22237?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1663765970236-f2acfde22237?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1663765970236-f2acfde22237?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   ];
-
   const videos = [
+    'https://videos.pexels.com/video-files/3773486/3773486-hd_1920_1080_30fps.mp4',  
     'https://videos.pexels.com/video-files/3773486/3773486-hd_1920_1080_30fps.mp4', 
-    'https://videos.pexels.com/video-files/3773486/3773486-hd_1920_1080_30fps.mp4',
-    'https://videos.pexels.com/video-files/3773486/3773486-hd_1920_1080_30fps.mp4'
+    'https://videos.pexels.com/video-files/3773486/3773486-hd_1920_1080_30fps.mp4', 
   ];
   const customTexts = [
     "the interior design ",
@@ -21,24 +18,15 @@ const DirectionAwareHoverDemo = () => {
     "Custom design",
   ];
 
-  // Calculate the maximum height of the cards
-  // const maxCardHeight = Math.max(
-  //   ...Array.from(document.querySelectorAll(".direction-aware-hover")).map(
-  //     (card) => card.getBoundingClientRect().height
-  //   )
-  // );
 
   return (
     <>
-   <p className="bg-[#e2e0dc] flex flex-col   items-center justify-center p-4 h-full">
-  <h1 className="font-semibold text-4xl font-serif text-black mb-4">Explore Our Latest Projects.</h1>
-  <h2 className="font-medium text-4xl font-serif text-slate-700 mb-10 text-center">
-    Discover groundbreaking AI solutions that are transforming 
-    industries and driving progress. <br/>
-    Step into the future with our cutting-edge projects.
-  </h2>
-</p>
+    <div className="bg-[#e2e0dc] flex  flex-col items-center p-4 ">
 
+    <h1 className="font-semibold text-2xl font-serif text-black">Explore Our Latest Projects.</h1>
+    <br/>
+    <h2 className="font-medium font-serif text-slate-700">Discover groundbreaking AI solutions that are transforming industries and driving progress. Step into the future with our cutting-edge projects.</h2>
+    </div>
   
     <div className="h-[100%]     bg-[#e2e0dc] mb-4 flex items-center align-middle justify-center">
     
@@ -52,7 +40,8 @@ const DirectionAwareHoverDemo = () => {
             
           
           >
-            <p className="font-bold  relative top-[-8rem] text-lg">{customTexts[index]}</p>
+              <p className="font-bold  relative top-[-8rem] text-xl"> {customTexts[index]} </p>
+           
           </DirectionAwareHover>
         ))}
         <div className="bg-transparent relative top-[-2rem] bottom-[1rem]  " >
