6da472abd06c1fd8d4bbf9e4a38546fab0c4ef40
dexterdotbuzz <90846543+dexterdotbuzz@users.noreply.github.com>
2024-05-15 22:17:53 +0800

    test

--- src/Components/Directionhover/Directionhver.tsx
+++ src/Components/Directionhover/Directionhver.tsx
@@ -3,7 +3,7 @@ import { CardStackDemo } from "../Cardstack/Cardstack";
 
 const DirectionAwareHoverDemo = () => {
   const images = [
-    "https://images.unsplash.com/photo-1663765970236-f2acfde22237?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://res.cloudinary.com/dzo4lqjb2/video/upload/v1715756057/Websites/fd-case3_1_wfedcz.mp4",
     "https://images.unsplash.com/photo-1663765970236-f2acfde22237?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://images.unsplash.com/photo-1663765970236-f2acfde22237?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   ];
