9799b8486737f6e911c3c234780f667952cc3432
dexterdotbuzz <90846543+dexterdotbuzz@users.noreply.github.com>
2024-05-22 22:49:57 +0800

    Update Header.tsx

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,24 +2,20 @@
 
 
 import { FiArrowRight } from "react-icons/fi";
-import logo from "../../assets/logo.svg";
+import logo from "../../public/logo.svg";
 
 const Header = () => {
-  return (
-    <header className="flex justify-between bg-[#E2E0DB] items-center px-4 py-1  text-slate-700">
-      <a href="/" className="p-2">
-        <img
-          className="h-8 w-auto"
-          src={logo}
-          alt="logo"
-        />
-      </a>
+return (
+<header classname="hidden md:flex justify-between bg-[#E2E0DB] items-center px-4 py-1 text-slate-700">
+<a href="/" classname="p-2">
+<img classname="h-8 w-auto" src alt="logo" />
+</a>
 
-      <a href="https://app.feeldesign.ai" className="p-2 font-semibold hover:text-gray-800">
-        <span>Login <FiArrowRight className="inline-block" /></span>
-      </a>
-    </header>
-  );
+<a href="https://app.feeldesign.ai" classname="p-2 font-semibold hover:text-gray-800">
+<span>Login <FiArrowRight className="inline-block" /></span>
+</a>
+</header>
+);
 };
 
 export default Header;
