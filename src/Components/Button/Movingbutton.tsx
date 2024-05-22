a9584c273439b3ff1501a10a50a3ae531b849cc7
dexterdotbuzz <90846543+dexterdotbuzz@users.noreply.github.com>
2024-05-22 22:12:25 +0800

    Update Movingbutton.tsx
    contact us button

--- src/Components/Button/Movingbutton.tsx
+++ src/Components/Button/Movingbutton.tsx
@@ -4,15 +4,15 @@ import { Button } from "../ui/moving-border";
 export function MovingBorderDemo() {
   return (
     <div className="mt-14 font-silk-serif">
-      <a href="https://app.feeldesign.ai">
-
-      <Button
-        borderRadius="1.75rem"
-        className="  bg-[#D1CFC8] font-bold text-stone-400 dark:text-stone-600 border-neutral-200 dark:border-transparent"
-        >
-        Get Started
-      </Button>
-        </a>
+    <a href="https://app.feeldesign.ai">
+    
+    <Button
+    borderRadius="1.75rem"
+    className=" bg-[#806B5A] font-silk-serif font-bold text-stone-100 dark:text-stone-600 border-neutral-200 dark:border-transparent"
+    >
+    Get Started
+    </Button>
+    </a>
 
 
 
